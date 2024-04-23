42188450dffdd4bcd8e27308a117b30b6d88515e
hkdas5464 <hkdas5464@gmail.com>
2024-04-23 09:50:52 +0530

    added mongooes

---
+++ src/models/userModel.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+import { type } from "os";
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: [true, "Please provide a username"],
+        unique: true
+    },
+    email: {
+        type: String,
+        required: [true, "Please provide a email"],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, "Please provide a password"],
+    },
+    isVerified: {
+        type: Boolean,
+        default: false
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    },
+    forgotPasswordTocken: String,
+    forgotPasswordDate: Date,
+    verifyToken: String,
+    verifyTockenExpiry: Date
+})
